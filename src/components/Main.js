b51d3c9785cc819cac9f8ecb8c7f012bed34ed94
Daniel JS <daniel@mydeveloperjourney.com>
2022-06-13 15:32:49 -0500

    wip: adds chnges

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,6 @@
+// react core functionality (hooks)
+import { useState, useEffect } from 'react';
+
 // component libraries
 import { Route } from 'react-router-dom';
 
@@ -6,6 +9,21 @@ import Index from '../pages/Index';
 import Show from '../pages/Show';
 
 const Main = (props) => {
+    
+    const [ people, setPeople ] = useState(null);
+
+    const URL = 'http://localhost:4000/people';
+
+    const getPeople = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setPeople(data);
+    }
+
+    useEffect(() => {
+        getPeople();
+    }, []);
+
     return (
         <main>
             <Route exact path="/">
