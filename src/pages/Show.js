70aa9743756239e66ffef5688c479ee6e641406e
Daniel JS <daniel@mydeveloperjourney.com>
2022-06-14 09:42:03 -0500

    wip: finish form inputs

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+
 const Show = (props) => {
-    const avatarURL = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png";
+    
+    const avatarURL = 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png';
     const id = props.match.params.id;
     const person = props.people.find(p =>  p._id === id); 
+    
+    const [ editForm, setEditForm ] = useState(person);
+
+    const handleChange = (event) => {};
+    const handleSubmit = (event) => {};
+
     return (
         <div className="person">
             <h1>{person.name}</h1>
@@ -11,6 +20,25 @@ const Show = (props) => {
                 ? <img src={person.image} alt={person.name} /> 
                 : <img src={avatarURL} alt="placeholder" /> 
             }
+            <form>
+                <input 
+                    name="name"
+                    value={editForm.name}
+                    onChange={handleChange} 
+                    type="text" 
+                />
+                <input
+                    name="title"
+                    value={editForm.title}
+                    onChange={handleChange}
+                    type="text" />
+                <input
+                    name="image"
+                    value={editForm.image}
+                    onChange={handleChange}
+                    type="text" />
+                <input type="submit" value="Update" />
+            </form>
         </div>
     );
 };
